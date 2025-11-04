f4c040fce0ae1a4e84f33237efb99938f470e20f
Francisco Sequeira <franciscosequeira879@gmail.com>
2025-11-04 14:13:32 -0300

    modificacion del project para agregar un filtrado por pais

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,12 +7,20 @@ import ProjectCard from "@/components/projects/ProjectCard";
 
 export default function ProjectsPage() {
   const [projects, setProjects] = useState<IProject[]>([]);
+  const [country, setCountry] = useState(""); 
+  const [countries, setCountries] = useState<string[]>([]); 
 
-  useEffect(() => {
+ useEffect(() => {
     const fetchProjects = async () => {
       try {
         const allProjects = await getAllProjects();
         setProjects(allProjects);
+
+       
+        const uniqueCountries = Array.from(
+          new Set(allProjects.map((p: IProject) => p.country).filter(Boolean))
+        );
+        setCountries(uniqueCountries);
       } catch (err) {
         console.error("Error trayendo proyectos", err);
       }
@@ -20,14 +28,33 @@ export default function ProjectsPage() {
     fetchProjects();
   }, []);
 
+  
+  const filteredProjects = country
+    ? projects.filter((p) => p.country === country)
+    : projects;
+
   return (
     <section>
       <div className="relative bg-gray-soft px-7 lg:pt-20 lg:px-30 lg:pb-10">
         <div className="absolute inset-0 bg-black/10 z-0"></div>
         <div className="relative z-10">
-          {projects.map((projectItem) => (
+          <select
+            value={country}
+            onChange={e => setCountry(e.target.value)}
+            style={{ marginBottom: "1rem" }}
+          >
+            <option value="">Todos los países</option>
+            {countries.map(c => (
+              <option key={c} value={c}>
+                {c}
+              </option>
+            ))}
+          </select>
+
+           {filteredProjects.map((projectItem) => (
             <ProjectCard project={projectItem} key={projectItem.title} />
           ))}
+        
         </div>
       </div>
     </section>
